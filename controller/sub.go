f0f6e2d8578e1dfda92c0249c9b5834d56607d6e
ashwini <ashwini_l@hcl.com>
2023-07-11 21:20:03 +0530

    sub feature

---
+++ controller/sub.go
@@ -0,0 +1,35 @@
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type Numbers struct {
+	First  int `json:"First"`
+	Second int `json:"Second"`
+}
+type Response struct {
+	Result int `json:"res"`
+}
+
+func Sub(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+	var num Numbers
+	err := json.NewDecoder(r.Body).Decode(&num)
+	if err != nil {
+		fmt.Println("Error in decoding JSON")
+	}
+	res := subtraction(num.First, num.Second)
+	response := Response{Result: res}
+	user, _ := json.Marshal(response)
+	w.Write(user)
+}
+
+func subtraction(a, b int) int {
+	sub := a - b
+	return sub
+}
--- main.go
+++ main.go
@@ -1,87 +1,25 @@
 package main
 
 import (
-	"encoding/json"
-
+	"calculator/controller"
 	"fmt"
-
 	"log"
-
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
-
-type AddNum struct {
-	Val1 int `json:"value1"`
-
-	Val2 int `json:"value2"`
-}
-
-type Sum struct {
-	Sum int `json:"sum"`
-}
 
 func main() {
 
-	fmt.Println("Connection Established")
+	fmt.Println("Server is started...")
+	router := httprouter.New()
 
-	handleRequests()
+	// Subtraction route
+	router.GET("/sub", controller.Sub)
 
-}
-
-func homepage(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Fprintf(w, "homepage")
-
-}
-
-func handleRequests() {
-
-	http.HandleFunc("/", homepage)
-
-	http.HandleFunc("/add", AdditionHandler)
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
-
-}
-
-func AdditionHandler(w http.ResponseWriter, r *http.Request) {
-
-	var addNum AddNum
-
-	if err := json.NewDecoder(r.Body).Decode(&addNum); err != nil {
-
-		http.Error(w, "Invalid Request", http.StatusBadRequest)
-
-		return
-
-	}
-
-	sum := calculateSum(addNum.Val1, addNum.Val2)
-
-	result := Sum{Sum: sum}
-
-	response, err := json.Marshal(result)
-
+	// Start HTTP Listener
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
-
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-
-		return
-
+		log.Printf("HTTP Server stopped - %s", err)
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write(response)
-
-}
-
-func calculateSum(addNum1, addNum2 int) int {
-
-	sum := 0
-
-	sum = addNum1 + addNum2
-
-	return sum
-
 }
