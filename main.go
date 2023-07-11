04068b708a8c8fa35d304a7569655f1015dc8463
Affan <hameedaffan3303@gmail.comm>
2023-07-11 18:43:58 +0530

    Subtraction file

--- main.go
+++ main.go
@@ -4,7 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
+type Numbers struct{
+	First int `json:"First"`
+	Second int `json:"Second"`
+}
+type Response struct{
+	Result int `json:"res"`
+}
 
 func homepage(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w,  "homepage")
@@ -12,10 +20,30 @@ func homepage(w http.ResponseWriter, r *http.Request)  {
 
 func handleRequests() {
 	http.HandleFunc("/",homepage)
+	http.HandleFunc("/sub",sub)
 	log.Fatal(http.ListenAndServe(":8081",nil))
+}
+
+func sub(w http.ResponseWriter, r *http.Request){
+	
+	var num Numbers
+	err:=json.NewDecoder(r.Body).Decode(&num)
+	if err!=nil{
+		fmt.Println("Error in decoding JSON")
+	}
+	res:=subtraction(num.First,num.Second)
+	response:=Response{Result:res}
+	user,_:=json.Marshal(response)
+	w.Write(user)
+	
+}
+func subtraction(a,b int) int{
+	sub:=a-b
+	return sub
 }
 
 func main() {
 	fmt.Println("hhhh")
 	handleRequests()
 }
+
