c9570997c5dfd1e7ee875ad0cf06addfb609bb30
Akhil151 <kanugulaakhila30@gmail.com>
2023-07-11 16:44:47 +0530

    adding n number

--- main.go
+++ main.go
@@ -1,21 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	//"fmt"
 	"log"
 	"net/http"
+	//"strconv"
 )
 
-func homepage(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w,  "homepage")
+type Numbers struct {
+	Values []int `json:"values"`
 }
 
-func handleRequests() {
-	http.HandleFunc("/",homepage)
-	log.Fatal(http.ListenAndServe(":8081",nil))
+type Result struct {
+	Sum int `json:"sum"`
 }
 
 func main() {
-	fmt.Println("hhhh")
-	handleRequests()
+	http.HandleFunc("/sum", sumHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+func sumHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var numbers Numbers
+	if err := json.NewDecoder(r.Body).Decode(&numbers); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	sum := calculateSum(numbers.Values)
+	result := Result{Sum: sum}
+
+	response, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
+
+func calculateSum(numbers []int) int {
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
+}
+
+
