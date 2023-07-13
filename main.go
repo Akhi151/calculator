4f509f9ec66e514c7d1b61cf1563272007512660
ashwini <ashwini_l@hcl.com>
2023-07-13 22:43:24 +0530

    Docker file added to sub branch

--- main.go
+++ main.go
@@ -16,11 +16,19 @@ func main() {
 
 	// Subtraction route
 	router.GET("/sub", controller.Sub)
+	//Addition route
 	router.GET("/add", controller.Add)
+
+	router.GET("/", welcome)
 
 	// Start HTTP Listener
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Printf("HTTP Server stopped - %s", err)
 	}
+}
+
+func welcome(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+	fmt.Fprintf(w, "homepage")
 }
